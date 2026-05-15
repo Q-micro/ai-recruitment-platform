852dec05fb97661c54e7d4496e77125a76b18d00
Q <202001256@student.polytechnic.bh>
2026-05-15 17:34:33 +0300

    serve frontend from express-2

--- server/index.js
+++ server/index.js
@@ -171,7 +171,7 @@ app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
   res.json({ message: "Server is running" });
 });
 
